b77f226ae8f110e16668bb5579a733a8b1e4b423
Vikrant Kumar <BHARDWAZVIKRANT83@GMAIL.COM>
2023-08-29 10:22:32 +0530

    form added to the application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 import Card from "./components/Card";
+import NewExpense from "./components/NewExpense";
 
 function App() {
   const expenses = [
@@ -13,6 +14,7 @@ function App() {
   return (
     <Card>
       <h2>lets get started</h2>
+      <NewExpense/>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
+import React,{useState} from 'react'
 function ExpenseItem(props) {
+  const [title,setTitle]=useState(props.title)
   const clickHandler = () =>{
-    console.log("clicked");
+    setTitle("updated");
   }
    
    
@@ -12,7 +14,7 @@ function ExpenseItem(props) {
         <ExpenseDate date={props.date}/>
       
       <div className="expense-item__description">
-        <h2>{props.title}</h2> <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2> <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick = {clickHandler}>Change Title</button>
     </Card>
