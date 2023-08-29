1633b604b0c057676ede96d403f0964cafd92da8
Vikrant Kumar <BHARDWAZVIKRANT83@GMAIL.COM>
2023-08-29 08:38:39 +0530

    button added for event handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
+import Card from "./components/Card";
 
 function App() {
   const expenses = [
@@ -10,13 +11,13 @@ function App() {
     { title: "batteries", amount: "40", date: new Date(2022, 4, 22) },
   ];
   return (
-    <div>
+    <Card>
       <h2>lets get started</h2>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,21 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 function ExpenseItem(props) {
+  const clickHandler = () =>{
+    console.log("clicked");
+  }
+   
    
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+    <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+      
       <div className="expense-item__description">
         <h2>{props.title}</h2> <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick = {clickHandler}>Change Title</button>
+    </Card>
   );
 }
 export default ExpenseItem;
